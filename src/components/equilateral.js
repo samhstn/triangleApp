1916e5a83689e7271dc63de5bae3d6cad81ae22b
shouston3 <shouston3@me.com>
2016-04-20 12:34:39 +0100

    made a div rotate, will implement over the triangle sides

--- src/components/equilateral.js
+++ src/components/equilateral.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import Button from './common/button.js'
 import TextBox from './common/textbox.js'
-import EdgeCover from './common/edgecover.js'
+require('../../public/style.css')
 
 export default class Equilateral extends Component {
   render () {
@@ -11,6 +11,15 @@ export default class Equilateral extends Component {
         <h1>Equilateral page</h1>
         <Link to='/'><h3>Back to Home</h3></Link>
         <div className='triangleContainer'>
+          <div
+            style={{
+              width: '200px',
+              height: '100px',
+              backgroundColor: 'orange',
+              transform: 'rotate(100deg)'
+            }}
+          >
+          </div>
           <TextBox name='bottomLeftCorner' borderColor='blue' top='330px' left='90px' />
           <TextBox name='topCorner' borderColor='red' top='110px' left='236px' />
           <TextBox name='bottomRightCorner' borderColor='green' top='330px' left='380px' />
@@ -20,15 +29,6 @@ export default class Equilateral extends Component {
           <Button name='top' color='red' left='230px' top='150px' />
           <Button name='left' color='blue' left='140px' top='310px' />
           <Button name='right' color='green' left='320px' top='310px' />
-          <EdgeCover name='topLeft' left='196px' top='184px' visibility='visible' />
-          <EdgeCover name='topLeft' left='178px' top='222px' visibility='visible' />
-          <EdgeCover name='topLeft' left='160px' top='260px' visibility='visible' />
-          <EdgeCover name='bottom' left='180px' top='310px' visibility='visible' />
-          <EdgeCover name='bottom' left='224px' top='310px' visibility='visible' />
-          <EdgeCover name='bottom' left='268px' top='310px' visibility='visible' />
-          <EdgeCover name='topRight' left='258px' top='184px' visibility='visible' />
-          <EdgeCover name='topRight' left='274px' top='222px' visibility='visible' />
-          <EdgeCover name='topRight' left='288px' top='260px' visibility='visible' />
           <img src='../../assets/equilateralTriangle.png'
             style={{
               position: 'absolute',
