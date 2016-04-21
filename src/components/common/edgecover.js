f77a05e2e7f4ad188f0560edbaa4f24d929d7a2c
shouston3 <shouston3@me.com>
2016-04-21 11:50:57 +0100

    the edges cover the triangle edges in equilateral.js

--- src/components/common/edgecover.js
+++ src/components/common/edgecover.js
@@ -3,19 +3,30 @@ import React, { Component } from 'react'
 export default class EdgeCover extends Component {
   render () {
     return (
-      <div name={this.props.name}
-        style={{
+      <div>
+        <div style={{
           zIndex: '1',
           position: 'absolute',
-          left: this.props.left,
-          top: this.props.top,
-          width: '60px',
-          height: '60px',
-          borderRadius: '50%',
-          backgroundColor: 'purple',
-          visibility: this.props.visibility
-        }}
-      ></div>
+          left: this.props.textLeft,
+          top: this.props.textTop,
+          width: '30px',
+          height: '20px'
+        }}>Side Length 
+        </div>
+        <div name={this.props.name}
+          style={{
+            zIndex: '1',
+            position: 'absolute',
+            left: this.props.left,
+            top: this.props.top,
+            width: '20px',
+            height: '150px',
+            backgroundColor: 'orange',
+            transform: 'rotate(' + this.props.angle + ')'
+          }}
+        ></div>
+      </div>
     )
   }
 }
+
