c660e5b430d83f0df88d7e3ca1d8adcd19df0984
shouston3 <shouston3@me.com>
2016-05-02 22:21:29 +0100

    refactored equilateral.js to not repeat components and refactored textboxes

--- src/components/equilateral.js
+++ src/components/equilateral.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import Corner from './common/corner.js';
-import TextBox from './common/textbox.js';
-import EdgeCover from './common/edgecover.js';
 import Edges from './equilateral/edges.js';
+import Corners from './equilateral/corners.js';
+import TextBoxes from './equilateral/textboxes.js';
 require('../../public/style.css');
 
 export default class Equilateral extends Component {
@@ -14,17 +13,10 @@ export default class Equilateral extends Component {
         <Link to='/'><h3>Back to Home</h3></Link>
         <div className='triangleContainer'>
           <Edges /> 
-          <TextBox name='bottomLeftCorner' borderColor='blue' top='330px' left='90px' />
-          <TextBox name='topCorner' borderColor='red' top='110px' left='236px' />
-          <TextBox name='bottomRightCorner' borderColor='green' top='330px' left='380px' />
-          <TextBox name='leftEdge' borderColor='purple' top='220px' left='130px' />
-          <TextBox name='rightEdge' borderColor='purple' top='220px' left='330px' />
-          <TextBox name='bottomEdge' borderColor='purple' top='380px' left='240px' />
-          <Corner name='top' color='red' left='230px' top='150px' textLeft='230px' textTop='70px' />
-          <Corner name='left' color='blue' left='140px' top='310px' textLeft='30px' textTop='345px' />
-          <Corner name='right' color='green' left='320px' top='310px' textLeft='430px' textTop='345px' />
+          <TextBoxes />
+          <Corners />
           <img src='../../assets/equilateralTriangle.png'
-            style={ styles }
+            style={styles}
           />
         </div>
       </div>
